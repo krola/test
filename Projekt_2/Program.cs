b891d01af62394cfe6da8cfb2dc624da316fed4e
paulaste <paulinastefaniakowa@gmail.com>
2019-05-17 22:36:29 +0200

    Add calc

--- Projekt_2/Program.cs
+++ Projekt_2/Program.cs
@@ -10,20 +10,37 @@ namespace Projekt_2
   {
     static void Main(string[] args)
     {
-      string name = Name();
-      string age = Age();
-      Console.WriteLine(name);
-      Console.WriteLine(age);
+      Console.WriteLine("Podaj liczbe pierwsza");
+      var A = Console.ReadLine();
+      Console.WriteLine("Podaj liczbe druga");
+      var B = Console.ReadLine();
+
+      var a = Convert.ToDouble(A);
+      var b = Convert.ToDouble(B);
+
+      double dodaj = Suma(a, b);
+      double odejmij = Roznica(a, b);
+      double pomnoz = Iloczyn(a, b);
+
+      Console.WriteLine("Suma");
+      Console.WriteLine(dodaj);
+      Console.WriteLine("Roznica");
+      Console.WriteLine(odejmij);
+      Console.WriteLine("Iloczyn");
+      Console.WriteLine(pomnoz);
+      Console.ReadLine();
     }
-     
-    static string Name()
+    static double Suma(double a, double b)
     {
-      return "Paulina";
+      return a + b;
     }
-    static string Age()
+    static double Roznica(double a, double b)
     {
-      return "29";
+      return a - b;
+    }
+    static double Iloczyn(double a, double b)
+    {
+      return a * b;
     }
   }
-
 }
