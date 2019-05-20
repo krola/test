b9312376721b82c4c616b56654b2ee60aeb8a5e3
paulaste <paulinastefaniakowa@gmail.com>
2019-05-20 16:48:00 +0200

    Add parity test

--- Projekt_3/Program.cs
+++ Projekt_3/Program.cs
@@ -10,6 +10,7 @@ namespace Projekt_3
   {
     static void Main(string[] args)
     {
+      //Zadanie 2 - liczenie z funkcja "if"
       Console.WriteLine("Podaj liczbe pierwsza");
       var C = Console.ReadLine();
       Console.WriteLine("Podaj liczbe druga");
@@ -20,22 +21,31 @@ namespace Projekt_3
       var c = Convert.ToDouble(C);
       var d = Convert.ToDouble(D);
 
-      double wynik;
-
       if (dzialanie == "+")
       {
-        wynik = c + d;
         Console.WriteLine(c + d);
       }
       if (dzialanie == "-")
       {
-        wynik = c - d;
         Console.WriteLine(c - d);
       }
       if (dzialanie == "*")
       {
-        wynik = c * d;
         Console.WriteLine(c * d);
+      }
+
+      //Zadanie 3 - metoda, która jako argument przyjmuje liczbę i zwraca true jeśli liczba jest parzysta
+      Console.WriteLine("Podaj dowolna liczbe, dla której sprawdzimy parzystość");
+      var Liczba = Console.ReadLine();
+      var liczba = Convert.ToInt32(Liczba);
+
+      if (liczba % 2 == 0)
+      {
+        Console.WriteLine("True");
+      }
+      else
+      {
+        Console.WriteLine("False");
       }
       Console.ReadLine();
     }
